c8c5cc6a420caaa69712cde4a4d1214977eb96aa
Gabe571 <gabriel.castro1028@gmail.com>
2020-10-14 11:19:54 -0700

    working controllers auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
     def login
         @user = User.find_by(username:params[:username])
-        
         if @user && @user.authenticate(params[:password])
             token = encode_token({user_id: @user.id})
             render json: {user:UserSerializer.new(@user), token:token}
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ CoffeeShop.destroy_all
 Favorite.destroy_all
 Review.destroy_all
 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -10,36 +11,7 @@ Review.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-gabriel = User.create(username:"gabriel", password:"123")
-    coffee_shops = [
-        {lat: 0,
-        lng: 0,
-        name: "white", 
-        address: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top",
-        zipcode: "85303",
-        description: "testing",
-        photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top"
-        }
-        ]
-    
-    coffee_shops.each {|coffee_shop| CoffeeShop.create(coffee_shop)}
-
-        favorites = [
-            {coffee_shop_id: 1,
-            user_id: 1,
-            }
-            ]
-            favorites.each {|favorite| Favorite.create(favorite)}
-
-     reviews = [
-            {reviewed_coffee: 1,
-            coffee_shop_id: 1,
-            user_id: 1, 
-            rating: 1 }
-            ]
-            
-                reviews.each {|review| Review.create(review)}
-            
+hal = User.create(username:"hal", password:"123")
         
 puts "seeds working!"
 
