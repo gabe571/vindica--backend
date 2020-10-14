9f5583f1f57caca0b37ca558d0114a97f93b5359
Gabe571 <gabriel.castro1028@gmail.com>
2020-10-14 14:23:59 -0700

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    validates :username, length: { minimum: 5 }
+    validates_associated :favorites
     has_many :favorites
     has_many :coffee_shops, through: :favorites
     has_secure_password 
